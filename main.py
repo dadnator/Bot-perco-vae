8271f05bbf69e78719587aa2fc81f11454fe6a9e
dadnator <massonhalima29@gmail.com>
2025-12-07 19:41:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ SETUP_IMAGE_URL = "https://i.imgur.com/8setyQq.png"
 ROLES_PING = {
     "Sleeping": {"id": 1446103551951638570, "label": " Sleeping", "emoji": "<:TheSleepingBlossoms:1446119822260965436>"},
     "La Bande": {"id": 1446104186533056684, "label": " La Bande", "emoji": "<:LABANDE:1446119877801672755>"},
-    "Skypiea'": {"id": 1446104186533056684, "label": " Skypiea'", "emoji": "☁️"},
+    "Skypiea'": {"id": 1447284168693383229, "label": " Skypiea'", "emoji": "☁️"},
 }
 }
 
